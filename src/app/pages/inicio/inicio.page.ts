fba120aef3ceb4ac152d380cd0c5b5822ac27bb0
Alexander Talavera <warrentalex26ies@gmail.com>
2019-05-06 13:59:37 -0600

    ion-input, ngmodel, passwords and email inputs

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -57,6 +57,11 @@ export class InicioPage implements OnInit {
       icon: 'infinite',
       name: 'Infinite Scroll',
       redirectTo: '/infinite-scroll'
+    },
+    {
+      icon: 'hammer',
+      name: 'Input - Forms',
+      redirectTo: '/input'
     }
   ];
 
