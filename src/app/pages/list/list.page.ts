ca4540d6c9a97edbf8a509270e73cf5bba1fa16a
Alexander Talavera <warrentalex26ies@gmail.com>
2019-05-06 14:23:40 -0600

    ion-item, creando servicio y peticion, pipe async

---
+++ src/app/pages/list/list.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../services/data.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.page.html',
+  styleUrls: ['./list.page.scss'],
+})
+export class ListPage implements OnInit {
+
+  usuarios: Observable<any>;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.usuarios = this.dataService.getUsers();
+  }
+
+}
