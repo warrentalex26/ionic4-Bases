d78ef740feb649820caf7e2422c1892e2c0ad61e
Alexander Talavera <warrentalex26ies@gmail.com>
2019-05-06 13:40:15 -0600

    infinte scroll

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -52,6 +52,11 @@ export class InicioPage implements OnInit {
       icon: 'grid',
       name: 'Grid - Rows',
       redirectTo: '/grid'
+    },
+    {
+      icon: 'infinite',
+      name: 'Infinite Scroll',
+      redirectTo: '/infinite-scroll'
     }
   ];
 
