7fc8a0169ac20f92348432a69001ae6fcf349764
Alexander Talavera <warrentalex26ies@gmail.com>
2019-05-06 15:00:30 -0600

    item sliding, viewchild para cerrar el slide, more

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {DataService} from '../../services/data.service';
 import {Observable} from 'rxjs';
+import {IonList} from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -9,12 +10,27 @@ import {Observable} from 'rxjs';
 })
 export class ListPage implements OnInit {
 
+  @ViewChild('lista') lista: IonList;
+
   usuarios: Observable<any>;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
+  }
+
+  favorite(evento) {
+    console.log('favorite', evento);
+    this.lista.closeSlidingItems();
+  }
+  share(evento) {
+    console.log('share: ', evento);
+    this.lista.closeSlidingItems();
+  }
+  borrar(evento) {
+    console.log('borrar: ', evento);
+    this.lista.closeSlidingItems();
   }
 
 }
